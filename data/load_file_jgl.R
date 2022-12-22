1b0c1ace0dfe36b1903ba1e1020d1ff1cc64d258
Gio Jacuzzi <giojacuzzi@gmail.com>
2022-12-22 10:42:33 -0800

    nit

--- data/load_file_jgl.R
+++ data/load_file_jgl.R
@@ -1,5 +1,6 @@
 ### Load data from JGL spl files
 source('global.R')
+library(readxl)
 
 get_name_from_file_jgl = function(file) {
   name = basename(file)
@@ -49,10 +50,10 @@ map_files_jgl_csv = function() {
   return(data_txt)
 }
 
-# get_file_map_nps = function() {
-#   return(read.csv('data/file_map_nps.csv'))
-# }
-# 
+get_file_map_jgl = function() {
+  return(read.csv('data/file_map_jgl.csv'))
+}
+
 # # Columns to subset from the raw data
 # selected_columns_nps = c(
 #   'STime',
@@ -97,46 +98,76 @@ map_files_jgl_csv = function() {
 #   'H16000',
 #   'H20000'
 # )
-# 
-# # Takes an absolute path to an SDA .XLS file
-# # Returns a list of data frames, one per date present in file
-# load_file_nps = function(file) {
-#   # Read data from .txt file
-#   data_failure = TRUE
-#   tryCatch({
-#     data_raw = read.csv(file)
-#     data_failure = FALSE
-#   }, error = function(e) {
-#     warning(paste('Unable to load data -', e$message, 'in', file))
-#   })
-#   if (data_failure) {
-#     return()
-#   }
-#   message(paste('Loading file', basename(file)))
-#   
-#   #   # Clean raw data (remove any labels and metadata)
-#   #   metadata_rows = which(data_raw$Place == 'Place')
-#   #   if (length(metadata_rows) > 0) data_raw = data_raw[-metadata_rows,] 
-#   
-#   # Validate site ID
-#   if (unique(data_raw$SiteID) != get_id_from_file_nps(file)) {
-#     stop('File site ID does not match measurement ID(s)!')
-#   }
-#   
-#   # Standardize time format
-#   data_raw$STime = as.POSIXct(data_raw$STime, paste(format_date,format_time), tz='UTC')
-#   
-#   # Subset data for desired measurements and rename time column
-#   data = data_raw[, selected_columns_nps]
-#   names(data)[names(data) == 'STime'] = 'Time'
-#   
-#   # Force data to 24-hour standardized format for each date recorded
-#   results = list()
-#   dates = unique(format(data$Time, format=format_date))
-#   for (date in dates) {
-#     data_date = data[format(data$Time, format=format_date) == date,]
-#     # data_date = fit_24hr_time_window(data_date)
-#     results = append(results, list(data_date))
-#   }
-#   return (results)
+
+# DEBUG FILE LOADING -------------------------------
+file = get_file_map_jgl()[1,'File']
+xlsx = readxl::read_excel(file)
+
+# Takes an absolute path to a 2019 JGL .xlsx file, returns a list containing a data frame
+# load_file_jgl = function(path) {
+  
+  # Read each A,B,C,... measurements page
+  
+  # # Read `Time History` measurements page
+  # data_failure = TRUE
+  # tryCatch({
+  #   data_raw = as.data.frame(readxl::read_excel(path, 'Time History'))
+  #   data_failure = FALSE
+  # }, error = function(e) {
+  #   warning(paste('Unable to load data -', e$message, 'in', path))
+  # })
+  # if (data_failure) {
+  #   # TODO: If NAVY, scrape any pre-calculated metrics from the 'Summary' sheet
+  #   return()
+  # }
+  # message(paste('Loading file', basename(path)))
+  # 
+  # # Clean raw data (remove any 'Run/Pause/Stop' metadata)
+  # measurement_rows = which(is.na(data_raw$`Record Type`))
+  # data = data_raw[measurement_rows,]
+  # 
+  # # Subset data for desired measurements
+  # data = data[, selected_columns_NAVY]
+  # 
+  # # TODO: May want to consider using multiple time series (ts) instead of simple vectors
+  # 
+  # # Validate date start
+  # date_start = format(data$Time[1], format=format_date)
+  # if (is.na(as.Date(as.character(data$Time[1]), tz = 'UTC', format = format_date))) {
+  #   # Scrape date from filename
+  #   date_start_malformatted = date_start
+  #   date_start = get_date_from_file_navy(path)
+  #   warning(paste('Date', date_start_malformatted, 'in unexpected format. Assuming 00:00:00 start on', date_start, 'instead.'))
+  #   
+  #   data$Time = seq(
+  #     from=as.POSIXct(paste(date_start, '00:00:00'), paste(format_date,format_time), tz='UTC'),
+  #     length.out=length(data$Time),
+  #     by='sec'
+  #   )
+  # } else if (any(date_start != format(data$Time, format=format_date))) {
+  #   warning(paste('Measured dates extend beyond start date', date_start))
+  # }
+  # 
+  # # # Validate time start
+  # # time_start = format(data$Time[1], format=format_time)
+  # # if (time_start != '00:00:00') {
+  # #   warning(paste('Measured start time (', time_start, ') is not 00:00:00', sep=''))
+  # # }
+  # # 
+  # # # Validate time measured (total number of seconds, assuming a 1 second frequency)
+  # # time_measured = length(data$Time)
+  # # 
+  # # hr = floor(time_measured / 3600)
+  # # min = floor((time_measured / 60) %% 60)
+  # # sec = time_measured %% 60
+  # # msg_time_measured = paste0('Total time measured (',hr,' hr ',min,' min ',sec,' sec)')
+  # # if (time_measured < time_24hr) {
+  # #   warning(paste0(msg_time_measured, ' is less than a full day'))
+  # # } else if (time_measured > time_24hr) {
+  # #   warning(paste0(msg_time_measured, ' is more than a full day'))
+  # # }
+  # 
+  # # Force data to 24-hour standardized format
+  # data = fit_24hr_time_window(data)
+  # return (list(data))
 # }
