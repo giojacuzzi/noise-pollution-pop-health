b8e46081e311764dd98051a281a49f35382184fe
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-01-24 17:05:28 -0800

    export MP1 flight ops from corresponding baseops, and convert to csv for investigation

---
+++ data/load_flight_operations.R
@@ -0,0 +1,2 @@
+# NOTE: The 'NASWI_MPX_Y_Noisemap - Flight Operations.xml' files have been exported from the corresponding .baseops case via BaseOps, where X is the monitoring period and Y is the file number (for Ault Field; Coupeville doesn't have this). These exports have been copied over to 'data/Noise Modeling Data/Exports/MP1', then opened in MS Excel, and saved as the .csv files used here.
+
