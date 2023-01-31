6a059357c647072b1e2223ef1b900019e87bdc6f
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-01-31 13:04:23 -0800

    fix analysis_flight_ops

--- analysis/analysis_flight_ops.R
+++ analysis/analysis_flight_ops.R
@@ -1,16 +1,17 @@
-data = read.csv('data/Noise Modeling Data/Aggregates/NASWI - Aggregate Flight Operations MP3 .csv')
+data = read.csv('data/Noise Modeling Data/Aggregates/NASWI - Aggregate Flight Operations MP3.csv')
 
 # Most active ops total
 n = 20
-data[order(-data$Num.Total)[1:n], c('Num.Total', 'Track.Group', 'Track.Type', 'Track', 'Runway', 'Profile')]
+data[order(-data$Num.Total)[1:n], c('Num.Total', 'Track.Group', 'Track.Type', 'Track', 'Runway', 'A.C.Category', 'Profile')]
 
 # Most active track types and groups
 levels(factor(data$Track.Type))
 ops_per_track_type  = round(tapply(X=data$Num.Total, INDEX=data$Track.Type, FUN=sum), 2)
 ops_per_track_group = round(tapply(X=data$Num.Total, INDEX=data$Track.Group, FUN=sum), 2)
+track_groups = unlist(labels(ops_per_track_group))
 df_track = data.frame(
-  Track.Group = unlist(labels(ops_per_track_group)),
-  Track.Type  = data[match(df_track$Track.Group, data$Track.Group), 'Track.Type'],
+  Track.Group = track_groups,
+  Track.Type  = data[match(track_groups, data$Track.Group), 'Track.Type'],
   Num.Total   = ops_per_track_group
 )
 rownames(df_track) = c()
