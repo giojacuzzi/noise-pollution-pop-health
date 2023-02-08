793d69d44b0f107cf8c38aad83391ed291d04a84
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-02-08 11:40:03 -0800

    plot noise count per period hourly

--- analysis/analysis_ops_and_events.R
+++ analysis/analysis_ops_and_events.R
@@ -22,21 +22,12 @@ events$Hour       = strftime(events$StartTime, format='%H')
 events$TimePeriod = get_time_period_for_hours(events$Hour)
 events$Day        = factor(weekdays(events$StartTime, abbreviate=T), levels=days)
 
-plots_events_per_site = list()
+plots_events_per_site_daily = list()
+plots_events_per_site_hourly = list()
 for (s in 1:nrow(sites)) {
   site = sites[s,'ID']
   events_site = events[events$SiteID==site,]
   events_site$MonitoringPeriod = as.factor(events_site$MonitoringPeriod)
-  # mean_events = nrow(events_site)/4
-  
-  # p = ggplot(events_site, aes(x=LAeq_Lmax, fill=MonitoringPeriod)) +
-  #   geom_histogram(binwidth=5, boundary=-2.5, position='identity', alpha = 0.5) +
-  #   xlim(25, 125) + ylim(0,1500) +
-  #   labs(title=paste0(site, ' (', sites[s,'Name'], ')'),
-  #        subtitle=paste('Mean events per week:', mean_events)) +
-  #   labs(x =ifelse((s==4 | s==9),'LAeq_Lmax',''),
-  #        y =ifelse((s==4 | s==9),'Number of events','')) +
-  #   theme(legend.position=ifelse((s==5 | s==11), 'right', 'none'))
   
   # Operations by weekday
   pdata_daily = data.frame()
@@ -45,7 +36,6 @@ for (s in 1:nrow(sites)) {
     for (day in days) {
       daily_ops = c(daily_ops, sum(events_site$Day==day & events_site$MonitoringPeriod==p))
     }
-
     pdata_daily = rbind(pdata_daily, data.frame(
       Day=factor(days, levels=days),
       NumOps=daily_ops,
@@ -56,15 +46,41 @@ for (s in 1:nrow(sites)) {
   
   p = ggplot(data=pdata_daily[order(pdata_daily$Day), ], aes(fill=MonitoringPeriod, color=MonitoringPeriod, group=MonitoringPeriod, x=Day, y=NumOps)) +
     geom_bar(position='dodge', stat='identity') +
-    labs(title=paste('Noise Events by Weekday -', site),
-         # subtitle=paste('Week period', period, 'Total:', sum(daily_ops)),
+    labs(title=paste(site),
          x ='Day',
          y ='Number of Events') +
     theme(legend.position=ifelse((s==5 | s==11), 'right', 'none'))
-  plots_events_per_site[[s]] = p
+  plots_events_per_site_daily[[s]] = p
+  
+  # Operations by hour
+  pdata_hourly = data.frame()
+  for (p in 1:4) {
+    hourly_ops = c()
+    for (hour in hours) {
+      hourly_ops = c(hourly_ops, sum(events_site$Hour==hour & events_site$MonitoringPeriod==p))
+    }
+    pdata_hourly = rbind(pdata_hourly, data.frame(
+      Hour=hours,
+      NumOps=hourly_ops,
+      TimePeriod=get_time_period_for_hours(0:23),
+      MonitoringPeriod=p
+    ))
+  }
+  pdata_hourly$MonitoringPeriod = as.factor(pdata_hourly$MonitoringPeriod)
+  
+  p2 = ggplot(data=pdata_hourly, aes(fill=MonitoringPeriod, color=MonitoringPeriod, group=MonitoringPeriod, x=Hour, y=NumOps)) +
+    geom_bar(position='dodge', stat='identity') +
+    labs(title=paste(site),
+         x ='Hour',
+         y ='Number of Events') +
+    theme(legend.position=ifelse((s==5 | s==11), 'right', 'none'))
+  plots_events_per_site_hourly[[s]] = p2
 }
 
-(plots_events_per_site[[1]] + plots_events_per_site[[2]] + plots_events_per_site[[3]]) / (plots_events_per_site[[4]] + plots_events_per_site[[5]] + plot_spacer())
+(plots_events_per_site_daily[[1]] + plots_events_per_site_daily[[2]] + plots_events_per_site_daily[[3]]) / (plots_events_per_site_daily[[4]] + plots_events_per_site_daily[[5]] + plot_spacer())
 
-(plots_events_per_site[[6]] + plots_events_per_site[[7]] + plots_events_per_site[[8]]) / (plots_events_per_site[[9]] + plots_events_per_site[[10]] + plots_events_per_site[[11]])
+(plots_events_per_site_daily[[6]] + plots_events_per_site_daily[[7]] + plots_events_per_site_daily[[8]]) / (plots_events_per_site_daily[[9]] + plots_events_per_site_daily[[10]] + plots_events_per_site_daily[[11]])
 
+(plots_events_per_site_hourly[[1]] + plots_events_per_site_hourly[[2]] + plots_events_per_site_hourly[[3]]) / (plots_events_per_site_hourly[[4]] + plots_events_per_site_hourly[[5]] + plot_spacer())
+
+(plots_events_per_site_hourly[[6]] + plots_events_per_site_hourly[[7]] + plots_events_per_site_hourly[[8]]) / (plots_events_per_site_hourly[[9]] + plots_events_per_site_hourly[[10]] + plots_events_per_site_hourly[[11]])
