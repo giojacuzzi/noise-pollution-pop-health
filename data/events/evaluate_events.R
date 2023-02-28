c04b12f3e0289d9e6f713b84c064f595c686421c
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-02-27 21:54:50 -0800

    towards event calculation

--- data/events/evaluate_events.R
+++ data/events/evaluate_events.R
@@ -57,19 +57,23 @@ ops = data_ops[data_ops$Date==date,]
 
 library(zoo)
 # movavg = function(x, n = 5){filter(x, rep(1 / n, n), sides = 2)}
+debug_hour = '20'
 
 my_events = data.frame()
 
+threshold_custom = 60 # Here, only look at events > 60 dB
+
 # 10-second moving average
 data$Lma = rollmean(data$LAeq, 10, align='center', fill=NA)
-for (hour in '21') {
+for (hour in debug_hour) {
   # NOTE: Navy threshold is L90 + 10 of each hour +/- 30 min 
   data_hour = data[data$Hour==hour,]
   L90 = LxFromLevels(data_hour$LAeq, 90)
-  threshold = 60 # Here, only look at events > 60 dB
+  threshold = threshold_custom # Use custom threshold instead
 
   sec = 1
   while (sec<=nrow(data_hour)) {
+    message(paste('sec',sec))
     while (is.na(data_hour$Lma[sec])) sec = sec + 1
     if (data_hour$Lma[sec] > threshold) {
       # Start event ---
@@ -91,16 +95,30 @@ for (hour in '21') {
       message(paste('END EVENT', data_hour$Time[sec]))
       event_end = sec
       idx_end = sec
-      idx_local_maxima = which(ggpmisc:::find_peaks(data_hour$Lma[idx_start:idx_end]))
-      # Only consider prominent maxima > L25 of the event itself
-      L25 = LxFromLevels(data_hour[idx_start:idx_end,'LAeq'], 25)
-      idx_local_maxima = idx_local_maxima[data_hour[idx_local_maxima, 'Lma'] >= L25]
+      idx_local_maxima = which(ggpmisc:::find_peaks(data_hour$Lma[idx_start:idx_end])) + idx_start - 1
+      # Filter only 'prominent' maxima > L25 of the event itself, and only maximums of level segments crossing over L25
+      # TODO: can segments have multiple maximums??
+      L25 = LxFromLevels(na.omit(data_hour[idx_start:idx_end,'Lma']), 25)
+      values_over = which(data_hour[idx_start:idx_end,'Lma']>=L25) + idx_start - 1
+      segments = cumsum(c(1, abs(values_over[-length(values_over)] - values_over[-1]) > 1))
+      segments = by(values_over, segments, identity)
+      peaks = c()
+      for (segment in segments) {
+        peaks = append(peaks, segment[which(data_hour[segment,'Lma']==max(data_hour[segment, 'Lma']))])
+      }
+
+      if (length(peaks) >  0) {
+        idx_local_maxima = peaks
+      } else {
+        # If no 'prominent' maximum, take the max peak
+        idx_local_maxima = idx_local_maxima[which(data_hour$Lma[idx_local_maxima]==max(data_hour$Lma[idx_local_maxima]))]
+      }
 
       # Plot the entire event with a +/- 10 sec buffer
       buff_start = max(1, idx_start-10)
       buff_end = min(nrow(data_hour), idx_end+10)
       p_time = ggplot(data_hour[buff_start:buff_end,]) +
-        labs(title='Found event') +
+        labs(title=paste('Event')) +
         geom_line(aes(x=Time, y=LAeq)) +
         geom_line(aes(x=Time, y=Lma), color='magenta') +
         geom_vline(xintercept=data_hour[idx_start,'Time'], color='blue') +
@@ -119,20 +137,20 @@ for (hour in '21') {
         for (i in 1:length(idx_local_maxima)) {
           message(paste('maxima', i))
           if (length(idx_local_maxima)>1) {
-            # Find end split. It becomes next start?
             idx_lmax = idx_local_maxima[i]
-            idx_end = ifelse(i<length(idx_local_maxima), idx_local_maxima[i+1], event_end)
-            # Wind to last local minimum
-            # while (idx_end > idx_start & data_hour$Lma[idx_end]<data_hour$Lma[idx_end-1]) {
-            #   idx_end = idx_end - 1
-            # }
-            idx_local_min = which(ggpmisc:::find_peaks(-data_hour$Lma[idx_start:idx_end]))
-            idx_end = idx_start + tail(idx_local_min, n=1) # Split point
+            if (i < length(idx_local_maxima)) {
+              # Find end split. It becomes next start.
+              idx_end = ifelse(i<length(idx_local_maxima), idx_local_maxima[i+1], event_end)
+              idx_local_min = which(ggpmisc:::find_peaks(-data_hour$Lma[idx_start:idx_end]))
+              idx_end = idx_start + tail(idx_local_min, n=1) # Split point
+            } else { # End of the super-event is the end of this final sub-event
+              idx_end = event_end
+            }
           }
 
           levels = data_hour$LAeq[idx_start:idx_end]
           lmax = max(levels)
-          idx_lmax = which(data_hour[idx_start:idx_end,'LAeq']==lmax)
+          idx_lmax = which(data_hour[idx_start:idx_end,'LAeq']==lmax)[1]
           lstart = data_hour$LAeq[idx_start]
           onset = (lmax - lstart)/(idx_lmax) # dBA per sec
           event = data.frame(
@@ -144,21 +162,21 @@ for (hour in '21') {
           my_events = rbind(my_events, event)
           message(paste('nrow event/my_events ', nrow(event), '/', nrow(my_events)))
 
-          # # Plot the sub-event with a +/- 10 sec buffer
-          # buff_start = max(1, idx_start-10)
-          # buff_end = min(nrow(data_hour), idx_end+10)
-          # p_time = ggplot(data_hour[buff_start:buff_end,]) +
-          #   labs(title=paste(nrow(my_events)),'(split)') +
-          #   geom_line(aes(x=Time, y=LAeq)) +
-          #   geom_line(aes(x=Time, y=Lma), color='magenta') +
-          #   geom_vline(xintercept=data_hour[idx_start,'Time'], color='blue') +
-          #   geom_vline(xintercept=data_hour[idx_start+idx_lmax-1,'Time'], color='red', linetype='dotted') +
-          #   geom_vline(xintercept=data_hour[idx_end,'Time'], color='blue') +
-          #   geom_hline(yintercept=threshold, color='gray') +
-          #   geom_hline(yintercept=LxFromLevels(data_hour[buff_start:buff_end,'LAeq'], 25), color='green') +
-          #   geom_vline(xintercept=data_hour[idx_local_maxima, 'Time'], color='orange', linetype='dotted') +
-          #   geom_vline(xintercept=data_hour[idx_local_min, 'Time'], color='turquoise', linetype='dotted')
-          # plot(p_time)
+          # Plot sub-events with a +/- 10 sec buffer
+            buff_start = max(1, idx_start-10)
+            buff_end = min(nrow(data_hour), idx_end+10)
+            p_time = ggplot(data_hour[buff_start:buff_end,]) +
+              labs(title=paste('Split event', nrow(my_events))) +
+              geom_line(aes(x=Time, y=LAeq)) +
+              geom_line(aes(x=Time, y=Lma), color='magenta') +
+              geom_vline(xintercept=data_hour[idx_start,'Time'], color='blue') +
+              geom_vline(xintercept=data_hour[idx_start+idx_lmax-1,'Time'], color='red', linetype='dotted') +
+              geom_vline(xintercept=data_hour[idx_end,'Time'], color='blue') +
+              geom_hline(yintercept=threshold, color='gray') +
+              geom_hline(yintercept=LxFromLevels(data_hour[buff_start:buff_end,'LAeq'], 25), color='green') +
+              geom_vline(xintercept=data_hour[idx_local_maxima, 'Time'], color='orange', linetype='dotted') +
+              geom_vline(xintercept=data_hour[idx_local_min, 'Time'], color='turquoise', linetype='dotted')
+            plot(p_time)
 
           idx_start = idx_end # Split point becomes start of next event
           if (idx_end >= event_end | idx_start >= event_end) {
@@ -177,26 +195,26 @@ for (hour in '21') {
 # Plot hour in 15 min chunks ------------------------
 
 
-for (hour in '21') { # TODO: hours
+for (hour in debug_hour) { # TODO: hours
   data_hour = data[data$Hour==hour,]
-  
+
   L50 = LxFromLevels(data_hour$LAeq, 50)
   L90 = LxFromLevels(data_hour$LAeq, 90)
   message(paste('Hour', hour, 'L90', L90))
-  # Navy threshold is L90 + 10 of each hour +/- 30 min 
-  thresh = L90 + 10
-  
+  # Navy threshold is L90 + 10 of each hour +/- 30 min
+  threshold_navy = L90 + 10
+
   events_hour = events[events$Hour==hour,]
   ops_hour = ops[ops$Hour==hour,]
-  
+
   for (q in 1:4) {
     start = (q-1)*(nrow(data_hour) / 4) + 1
     end = start + (nrow(data_hour) / 4) - 1
-    
+
     data_q = data_hour[start:end,]
     events_q = events_hour[events_hour$StartTime %in% data_hour[start:end, 'Time'],]
     ops_q = ops_hour[ops_hour$Time %in% data_hour[start:end, 'Time'],]
-    
+
     # ma = na.omit(data.frame(Time=data_q$Time, MA=movavg(data_q$LAeq)))
     ma = na.omit(data.frame(
       Time=data_q$Time,
@@ -210,13 +228,14 @@ for (hour in '21') { # TODO: hours
       geom_vline(xintercept=events_q$StartTime, color='red', linetype='dotted') +
       # Navy flight operation
       geom_vline(xintercept=ops_q$Time, color='blue', linetype='dashed') +
-      geom_hline(yintercept=thresh, color='gray') +
+      geom_hline(yintercept=threshold_navy, color='gray') +
+      geom_hline(yintercept=threshold_custom, color='green') +
       geom_line(ma, mapping=aes(x=Time, y=MA), color='magenta')
-    
+
     # Spectral heatmap
     spectrum = data_q[,c(1, 31:61)] # NOTE: 6.3 Hz starts at index 26
     names(spectrum) = gsub('1/3 LZeq ', '', names(spectrum))
-    
+
     spectrum_total = data.frame()
     for (s in 1:nrow(spectrum)) {
       sec = as.POSIXct(spectrum$Time[s])
@@ -235,12 +254,12 @@ for (hour in '21') { # TODO: hours
     spectrum_total$Band = factor(spectrum_total$Band)
     sorted_levels = as.character(sort(as.numeric(levels(spectrum_total$Band))))
     spectrum_total$Band = factor(spectrum_total$Band, levels=sorted_levels)
-    
+
     p_spectral = ggplot(spectrum_total, aes(x=Time, y=Band, fill=LZeq)) +
       geom_tile() +
       scale_fill_viridis(option='A') +
       labs(x='Time', y='Band')
-    
+
     print(p_spectral / p_leq)
   }
 }
