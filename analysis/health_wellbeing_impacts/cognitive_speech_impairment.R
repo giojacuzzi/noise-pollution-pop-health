89814bb150e0629f29732c13c7ae1b4dc4d79bab
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-06-15 12:49:11 -0700

    add flight tracks and todos

--- analysis/health_wellbeing_impacts/cognitive_speech_impairment.R
+++ analysis/health_wellbeing_impacts/cognitive_speech_impairment.R
@@ -49,7 +49,8 @@ schools = bind_rows(schools_affected, schools_unaffected)
 schools_affected$NAME
 mapview(Ldn_gt55, zcol='Level', layer.name='DNL') + mapview(population_bg, col.regions=list('white')) +
   mapview(schools, zcol='Ldn55', col.regions=list('gray', 'red')) +
-  mapview( sites_with_events, xcol='Longitude', ycol='Latitude', layer.name = 'Sites', crs=crs, grid=F)
+  mapview( sites_with_events, xcol='Longitude', ycol='Latitude', layer.name = 'Sites', crs=crs, grid=F) +
+  mapview(get_flighttracks())
 
 ## Cognitive development in children -------------------------------------------
 
@@ -63,13 +64,15 @@ unique(data_metrics[data_metrics$Lden>=55, 'ID'])
 
 # ANSI 12.3 states that the criteria for allowable background noise level can be relaxed for irregular noise events. This is because speech is impaired only for the short time when the aircraft noise is close to its maximum. Consequently, when the background noise level of the noisiest hour is dominated by aircraft noise, the indoor criteria can be increased. The Leq of 35 dB for continuous background noise can be increased by 5 dB to an Leq of 40 dB. However, the noise level cannot exceed 40 dB for more than 10% of the noisiest hour. This is for a room that's less than 20,000 cubic feet.
 
+# https://www.noisequest.psu.edu/noiseeffects-speech.html
 # SEL has been recommended by some as a better choice for estimating speech interference from aircraft overflights indoors. A maximum SEL of 64 dB is suggested. A 26 dB noise reduction is assumed when you move indoors from outdoors. So, a 64 dB SEL indoors is about equal to 90 dB SEL outdoors. Aircraft events with outdoor SEL values greater than 90 dB would disrupt indoor speech communication. The research indicates that speakers using a casual vocal effort can achieve 95% intelligibility when indoor SEL values did not exceed 60 dB. This translates to an approximately 50 dB Lmax.
 
-# TODO: https://www.noisequest.psu.edu/noiseeffects-speech.html
-nearby_sites = c('NwbH', '2B_T')
+# Measured SEL near schools
 # Near Coupeville Elementary and Coupeville High
-# TODO: exact distance
-summary(data_events[data_events$ID=='NwbH', 'SEL'])
+# TODO: exact distance, and only consider school hours
+data_events[data_events$ID=='NwbH' & data_events$SEL >= 90, ]
 # Near Crescent Harbor Elementary and Olympic View Elementary
-# TODO: exact distance
-summary(data_events[data_events$ID=='2B_T', 'SEL'])
+# TODO: exact distance, and only consider school hours
+data_events[data_events$ID=='2B_T' & data_events$SEL >= 90, ]
+
+# TODO: Modeled SEL for flight tracks near schools
